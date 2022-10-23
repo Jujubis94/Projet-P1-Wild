e3eda64351a7a22650c3ac7c7bfc2468de675497
Julien Marcenac <julien.marcenac2@gmail.com>
2022-10-23 15:32:36 +0200

    Add text content and btn top

--- script.js
+++ script.js
@@ -12,12 +12,15 @@ closeNav.addEventListener("click", () => {
   sidenav.classList.remove("active");
 });
 
-/*
-if(
-const naruto = document.querySelector("#idnaruto")){
-  
-}
-*/
+/* btnScroll*/
+const btntop = document.querySelector(".scroll_to_top");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    btntop.classList.add("visible");
+  } else {
+    btntop.classList.remove("visible");
+  }
+});
 
 let swiper = new Swiper(".swiper", {
   cssMode: true,
